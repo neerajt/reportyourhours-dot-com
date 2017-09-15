3022709ad918b9be9a32869d5fc6170fa98f7954
neerajt <neerajtandon25@gmail.com>
2017-09-14 23:30:43 -0500

    add api routes for hours and total_hours

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,6 +3,9 @@ const express = require('express'),
   Volunteer = require('../../../models/volunteer'),
   _ = require('lodash');
 
+const database_settings = require('../../../knexfile.js');
+
+const  knex = require('knex')(database_settings[process.env.NODE_ENV]);
 
 function setRoute(route, handler){
   return router.post(route, function(req, res, next){
@@ -24,6 +27,30 @@ setRoute('/volunteers', function(requestBody){
 
   return new Volunteer(data)
     .save();
+});
+
+function getHours(data){
+ _.map(data, 'hours') 
+};
+
+router.get('/totalhours', function(req, res, next){
+  knex.raw(`WITH hours AS(
+         SELECT jsonb_array_elements(log) as hours FROM volunteers
+        )
+      SELECT sum(abs((hours ->> 'hours')::float)) as total_hours FROM hours`)
+    .then(function(resp) {
+      res.send(resp.rows[0])
+      })
+});
+
+router.get('/hours', function(req, res, next){
+  knex.raw(`WITH hours AS(
+         SELECT jsonb_array_elements(log) as hours FROM volunteers
+        )
+      SELECT abs((hours ->> 'hours')::float) as hours FROM hours`)
+    .then(function(resp) {
+      res.send(resp.rows)
+      })
 });
 
 module.exports = router;
